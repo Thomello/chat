a76c3a24f7e1ae76c2b562ecdcd9fe953c306988
Nom Nom <thomellomatsasa@gmail.com>
2023-05-04 17:35:36 +0200

    Update server url

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async(e) => {
 
     // ftech data from the server
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://chat-uy2e.onrender.com', {
       method : 'POST',
       headers : {
         'Content-Type' : 'application/json'
